fd4a26c0b06ea54965fc6d0a29f589714264ad69
Joji-iidaka <joji.kapi.0218@gamil.com>
2024-05-21 22:42:59 +0900

    github pages対応&ログイン機能追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import React,{useState} from'react';
 import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
 import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <Router>
+  const [isAuth,setIsAuth] = useState(false);
+
+  return (
+  <Router>
+    <NavBar isAuth={isAuth}/>
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/createpost" element={<CreatePost />}></Route>
+      <Route path="/login" element={<Login setIsAuth={setIsAuth} />}></Route>
+      <Route path="/logout" element={<Logout setIsAuth={setIsAuth}/>}></Route>
     </Routes>
   </Router>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import './NavBar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse,
+        faFilePen,
+        faArrowRightToBracket
+        } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = ({isAuth}) => {
+  return (
+    <nav>
+        {/*  cssを当てる際はaタグを指定する */}
+        <Link to="/">
+            <FontAwesomeIcon icon={faHouse} />
+            ホーム
+        </Link>
+        <Link to="/createpost">
+            <FontAwesomeIcon icon={faFilePen}/>
+            記事投稿
+        </Link>
+        {!isAuth ? (
+            <Link to="/login">
+            <FontAwesomeIcon icon={faArrowRightToBracket}/>
+            ログイン
+            </Link>
+        ) : (
+            <Link to="/logout">
+            <FontAwesomeIcon icon={faArrowRightToBracket}/>
+            ログアウト
+            </Link>
+        )}
+    </nav>
+  );
+}
+
+export default NavBar
