6918a4cd642b42b88154d02b33eb5b50b21598f1
Joji-iidaka <joji.kapi.0218@gamil.com>
2024-05-22 23:21:54 +0900

    投稿登録＆投稿表示

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import './Home.css';
+import { collection,getDocs } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useEffect } from'react';
+import { useState } from'react';
 
 const Home = () => {
-  return <div>Home</div>
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () =>{
+      const data = await getDocs(collection(db,"posts"));
+      setPostList(data.docs.map((doc) => ({...doc.data(),id:doc.id })));
+    };
+    getPosts();
+  },[]);
+
+  return <div className='homePage'>
+    {postList.map((post) => {
+      return (
+        <div key={post.id} className='postContents'>
+        <div className='postHeader'>
+          <h1 >{post.title}</h1>
+        </div>
+
+        <div className='postTextContainer'>
+          {post.postText}
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@{post.auth.username}</h3>
+          <button>削除</button>
+        </div>
+      </div>
+      )
+    })}
+  </div>
 }
 
 export default Home;
